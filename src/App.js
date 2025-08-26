30ca1a295b0dcf9b559bb7754487a36e68c46f8c
marioStorelink <m.derrico@storelink.it>
2025-08-26 14:50:37 +0200

    Aggiunta file progetto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="hearts">
+        {Array.from({ length: 20 }).map((_, i) => (
+          <span key={i} className="heart">❤️</span>
+        ))}
+      </div>
+      <h1 className="love-text">L'amore è la magia che unisce i cuori</h1>
+    </div>
+  );
+}
+
